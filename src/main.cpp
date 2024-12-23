db5497366c727e7cec96ae25e0e7a231c244df7e
druckgott <forum@coolpixx.de>
2024-12-23 12:27:38 +0100

    State Machine

--- src/main.cpp
+++ src/main.cpp
@@ -45,7 +45,7 @@ uint16_t paletteDuration = 7; //sec
 
 const uint8_t PIN_LED_STATUS = 13;
 
-uint8_t brightnesses[] = { 255, 128, 64, 1 };
+uint8_t brightnesses[] = { 255, 128, 64, 0 };
 uint8_t currentBrightnessIndex = 0;
 
 uint8_t currentPatternIndex = 0; // Index number of which pattern is current
@@ -64,11 +64,21 @@ Button buttonBrightness(PIN_BUTTON_BRIGHTNESS);
 Button buttonPattern(PIN_BUTTON_PATTERN);
 Button buttonPalette(PIN_BUTTON_PALETTE);
 
+enum State {
+  STATE_NORMAL,
+  STATE_LOW_BATTERY,
+  STATE_SLEEP
+};
+
+State currentState = STATE_NORMAL;
+
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-void wakeUp()
-{
-  Serial.println("Waking up...");
+// ISR-Funktion im IRAM speichern
+void IRAM_ATTR wakeUp() {
+  // Code, der beim Interrupt ausgeführt wird
+  // Zum Beispiel einen Zustand setzen oder einen anderen Mechanismus auslösen
+  Serial.println("Wakeup triggered!");
 }
 
 void nextBrightness()
@@ -84,7 +94,7 @@ void nextPattern()
 {
   // add one to the current pattern number, and wrap around at the end
   currentPatternIndex = (currentPatternIndex + 1) % patternsCount;
-  Serial.print("Pattern: ");
+  Serial.print("#Pattern#: ");
   //Serial.println(currentPatternIndex);
   Serial.println(patternNames[currentPatternIndex]);
 }
@@ -93,7 +103,7 @@ void nextPalette()
 {
   // add one to the current palette number, and wrap around at the end
   currentPaletteIndex = (currentPaletteIndex + 1) % paletteCount;
-  Serial.print("Palette: ");
+  Serial.print("*Palette*: ");
   Serial.println(paletteNames[currentPaletteIndex]);
 }
 
@@ -117,32 +127,6 @@ float getBatteryVoltage() {
     //Serial.print("batteryVoltage: ");
     //Serial.println(batteryVoltage);
     return batteryVoltage;
-}
-
-void setup() {
-  Serial.begin(115200);
-  Serial.println("setup");
-  
-  delay(3000); // 3 second delay for recovery
-
-  setNumLeds(NUM_LEDS);
-  FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
-
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //  FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER,DATA_RATE_MHZ(12)>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-
-  // set master brightness control
-  FastLED.setBrightness(brightnesses[currentBrightnessIndex]);
-
-  FastLED.show(CRGB::Black);
-
-  buttonPattern.begin();
-  buttonPalette.begin();
-  buttonBrightness.begin();
-
-  pinMode(PIN_LED_STATUS, OUTPUT);
-  digitalWrite(PIN_LED_STATUS, LOW);
 }
 
 void handleInput() {
@@ -180,67 +164,145 @@ void handleInput() {
   }
 }
 
+void handleLowBattery() {
+  Serial.flush();
+  fill_solid(leds, NUM_LEDS, CRGB::Red);
+  FastLED.show();
+}
+
+void handleSleep() {
+  Serial.println("Going down...");
+  Serial.flush();
+
+  fill_solid(leds, NUM_LEDS, CRGB::Black);
+  FastLED.show();
+
+// Interrupts für Wakeup einrichten
+  pinMode(PIN_BUTTON_PATTERN, INPUT_PULLUP);  
+  pinMode(PIN_BUTTON_BRIGHTNESS, INPUT_PULLUP);  
+  attachInterrupt(PIN_BUTTON_PATTERN, wakeUp, FALLING);   // Flash-Button für Pattern-Wakeup
+  attachInterrupt(PIN_BUTTON_BRIGHTNESS, wakeUp, FALLING); // Brightness-Button für Wakeup
+  //attachInterrupt(PIN_BUTTON_PALETTE, wakeUp, FALLING);  // Optional: Palette-Button für Wakeup
+
+  // Deep Sleep einleiten (oder ein ähnlicher Stromsparmodus)
+  ESP.deepSleep(0); // Deep Sleep-Modus ohne Timer, wird durch Interrupt geweckt
+
+  // WICHTIG: Der Code nach deepSleep wird nur ausgeführt, wenn der ESP geweckt wird
+  Serial.println("I have awoken!");
+
+  // Interrupts deaktivieren, wenn sie nicht mehr benötigt werden
+  detachInterrupt(PIN_BUTTON_PATTERN);
+  detachInterrupt(PIN_BUTTON_BRIGHTNESS);
+  //detachInterrupt(PIN_BUTTON_PALETTE);
+
+  Serial.println("I have awoken!");
+  nextBrightness();
+}
+
+void handleNormalOperation() {
+  // Call the current pattern function once, updating the 'leds' array
+  patterns[currentPatternIndex]();
+  currentPalette = palettes[currentPaletteIndex];
+  FastLED.show();
+  FastLED.delay(1000 / FRAMES_PER_SECOND);
+
+  EVERY_N_MILLISECONDS(40) {
+    hue++;
+    nblendPaletteTowardPalette(currentPalette, targetPalette, 8);
+  }
+
+  EVERY_N_MILLISECONDS(4) {
+    hueFast++;
+  }
+
+  if (cyclePattern == 1 && (millis() > cyclePatternTimeout)) {
+    nextPattern();
+    cyclePatternTimeout = millis() + (cyclePatternDuration * 1000);
+  }
+
+  if (cyclePalette == 1 && (millis() > paletteTimeout)) {
+    nextPalette();
+    paletteTimeout = millis() + (paletteDuration * 1000);
+  }
+
+}
+
+State changeState(State newState) {
+  // Wenn sich der Zustand ändert, gebe eine Nachricht aus
+  if (newState != currentState) {
+    currentState = newState;
+    switch (newState) {
+      case STATE_LOW_BATTERY:
+        Serial.println("STATE_LOW_BATTERY");
+        break;
+      case STATE_SLEEP:
+        Serial.println("STATE_SLEEP");
+        break;
+      case STATE_NORMAL:
+        Serial.println("STATE_NORMAL");
+        break;
+      // Füge hier nach Bedarf weitere Fälle hinzu
+    }
+  }
+  return newState;
+}
+
+void updateState() {
+  if (getBatteryVoltage() <= SWITCH_OFF_VOLTAGE) {
+    currentState = changeState(STATE_LOW_BATTERY);
+  } else if (brightnesses[currentBrightnessIndex] == 0) {
+    currentState = changeState(STATE_SLEEP);
+  } else {
+    currentState = changeState(STATE_NORMAL);
+  }
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("setup");
+  
+  delay(3000); // 3 second delay for recovery
+
+  setNumLeds(NUM_LEDS);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5, 500);
+
+  // tell FastLED about the LED strip configuration
+  FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+
+  // set master brightness control
+  FastLED.setBrightness(brightnesses[currentBrightnessIndex]);
+
+  FastLED.show(CRGB::Black);
+
+  buttonPattern.begin();
+  buttonPalette.begin();
+  buttonBrightness.begin();
+
+  pinMode(PIN_LED_STATUS, OUTPUT);
+  digitalWrite(PIN_LED_STATUS, LOW);
+}
+
 void loop()
 {
-   if(getBatteryVoltage() <= SWITCH_OFF_VOLTAGE){
-    Serial.flush();
-    fill_solid(leds, NUM_LEDS, CRGB::Red);
-    FastLED.show();
-  }else{
-    // Call the current pattern function once, updating the 'leds' array
-    patterns[currentPatternIndex]();
-    currentPalette = palettes[currentPaletteIndex];
+  updateState();
 
-    // send the 'leds' array out to the actual LED strip
-    FastLED.show();
-    // insert a delay to keep the framerate modest
-    FastLED.delay(1000 / FRAMES_PER_SECOND);
+  switch (currentState) {
+    case STATE_LOW_BATTERY:
+      handleLowBattery();
+      break;
 
-    // do some periodic updates
-    EVERY_N_MILLISECONDS( 40 ) {
-      hue++;  // slowly cycle the "base color" through the rainbow
+    case STATE_SLEEP:
+      handleSleep();
+      break;
 
-      // slowly blend the current palette to the next
-      nblendPaletteTowardPalette(currentPalette, targetPalette, 8);
-    }
-
-    EVERY_N_MILLISECONDS( 4 ) {
-      hueFast++;  // slowly cycle the "base color" through the rainbow
-    }
-
-    if (cyclePattern == 1 && (millis() > cyclePatternTimeout)) {
-      nextPattern();
-      cyclePatternTimeout = millis() + (cyclePatternDuration * 1000);
-    }
-
-    if (cyclePalette == 1 && (millis() > paletteTimeout)) {
-      nextPalette();
-      paletteTimeout = millis() + (paletteDuration * 1000);
-    }
-
-    handleInput();
+    case STATE_NORMAL:
+    default:
+      handleNormalOperation();
+      break;
   }
- 
 
-  if (brightnesses[currentBrightnessIndex] == 0) {  
-    Serial.println("Going down...");
-    Serial.flush();
-  
-    fill_solid(leds, NUM_LEDS, CRGB::Black);
-    FastLED.show();
-    
-    // Allow wake up pin to trigger interrupt on low.
-    attachInterrupt(0, wakeUp, LOW);
-    attachInterrupt(1, wakeUp, LOW);
-
-    // Disable external pin interrupt on wake up pin.
-    detachInterrupt(0);
-    detachInterrupt(1);
-  
-    Serial.println("I have awoken!");
-    
-    nextBrightness();
-  }
+  // Verarbeite Benutzereingaben, falls der Zustand dies zulässt
+  handleInput();
 }
 
 
